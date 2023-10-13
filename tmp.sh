fc43664efcb3146850d4b31874a003a3887005a8
Giang <giang@gpu5.cse.eng.auburn.edu>
2023-10-13 18:52:39 +0000

    update

--- tmp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-shuf -n 1100 -e /home/giang/Downloads/RN50_dataset_CUB_HP/merged/*/*.jpg | xargs -I{} mv {} /home/giang/Downloads/RN50_dataset_CUB_HP/train_tmp/
-shuf -n 200 -e /home/giang/Downloads/RN50_dataset_CUB_HP/merged/*/*.jpg | xargs -I{} mv {} /home/giang/Downloads/RN50_dataset_CUB_HP/val_tmp/
